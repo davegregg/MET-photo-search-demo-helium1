4758c1bcd8af3a63a88366f342de242864bc8626
davegregg@gmail.com <davegregg@gmail.com>
2021-08-02 14:01:33 -0400

    Helium 1 supplement demo code

--- main.js
+++ main.js
@@ -1,39 +1,71 @@
-// Synchronous code
-
-// "sync": together, aligned, sequential, matched up, step-by-step, harmony, accord
-// "chrono": time
-
-// Asynchronous code
-// "A-" means "not"
-
-// So...
-"Synchronous code executes reliably, one after the other."
-"Asynchronous code will run LATER, perhaps even at an unknown time."
-
-console.log("First in order")
-setTimeout(() => console.log("Seventh in order"), 5000)
-setTimeout(() => console.log("Third in order"), 0)
-setTimeout(() => console.log("Fourth in order"), 0)
-setInterval(() => {}, 0) // Async code is non-blocking. This is an infinite loop which DOESN'T block other things from happening.
-setTimeout(() => console.log("Fifth in order"), 0)
-setTimeout(() => console.log("Sixth in order"), 1000)
-console.log("Second in order")
-
 let mainElement = document.querySelector("main")
-let searchTextbox = document.querySelector("#search-text")
-let searchButton = document.querySelector("#search")
+let searchForm = document.querySelector("#search")
+let searchInput = document.querySelector("#search input")
 
-searchButton.addEventListener("click", () => {
-    let searchTerm = searchTextbox.value.toLowerCase()
-    
-    fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`)
-        .then(response => response.json()) 
-        .then(pokemon => {
-            console.log(pokemon)
-            
-            let imgElement = document.createElement("img")
-            imgElement.src = pokemon.sprites.front_default
+/*
+    1. When the user submits the search form, we will get their search term ready.
+    2. Then we will trigger the first fetch() request, which will use the user's
+       search term to search the Metropolitan Museum of Art's API for museum objects
+       which best match that search term.
+*/
 
-            mainElement.append(imgElement)
-        })
+searchForm.addEventListener("submit", event => {
+    event.preventDefault() // <form> normally does some stuff we don't want.
+
+    let searchTerm = searchInput.value.toLowerCase() // Preparing user input
+    fetchTopSearchResult(searchTerm) // Let's trigger our first fetch() request
 })
+
+/*
+    Because the `/public/collection/v1/search` only gives us an array of IDs,
+    our task is:
+    1. First, fetch the ID of the top search result,
+    2. and THEN, fetch more information for the object which has that ID,
+        which we will do by running our fetchMuseumObjectDetails() function.
+
+    By the end, we will have two fetch() requests. These are easier to manage when
+    we put them inside their own functions.
+*/
+
+function fetchTopSearchResult (searchTerm) {    
+    //             domain---------------------endpoint--------------------query parameters
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${searchTerm}`)
+        .then(response => response.json())
+        .then(data => {
+            let topResultID = data.objectIDs[0] /* By reading the API documentation and console
+                                                   logging the `data` object, we learned that it
+                                                   contains an `objectIDs` property, which is an
+                                                   array of IDs. We have decided that we just want
+                                                   the top search result, so we only need the
+                                                   first ID in the array. */
+
+            fetchMuseumObjectDetails(topResultID) /* Instead of writing the SECOND fetch() within
+                                                     this first fetch(), we will write it in a
+                                                     function with a readable name, and trigger
+                                                     that function from here. This is a best-
+                                                     practice, because it keeps our code much 
+                                                     more manageable. */ 
+        })    
+}
+
+function fetchMuseumObjectDetails (id) {
+    /*
+        Now that we have the ID for identifying which museum "object" we want to get more
+        information about, we can plug that ID into another endpoint we saw in the API docs
+        in order to get _details_ about it.
+    */
+
+    fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/" + id)
+        .then(response => response.json())
+        .then(museumObject => {
+
+            let imgElement = document.createElement("img")
+            imgElement.src = museumObject.primaryImageSmall /* We know this is the right property
+                                                               for getting the image URL, because
+                                                               we spotted it in the console.log()
+                                                               and we saw it in the API docs. */
+
+            mainElement.prepend(imgElement)
+
+        })
+}
